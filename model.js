336d8c3b010de0992874b5b4a1c2754e07a8033e
Philip Denton <Phil@philips-mbp.home>
2023-06-30 14:42:19 +0100

    updated pull request notes

--- model.js
+++ model.js
@@ -57,25 +57,6 @@ exports.selectArticles = (searchByTopic = undefined, sortBy = 'created_at', orde
     } else {
         return Promise.reject({status: 404, msg: "Request Not Found"})
     }
-    
-    
-
-
-
-    // const queryValues = []
-    // let queryStr = `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id`
-    // if(searchByTopic){
-    //     queryValues.push(searchByTopic)
-    //     queryStr += ` WHERE articles.topic = $1`  
-    // }
-    // queryStr += ` GROUP BY articles.article_id ORDER BY ${sortBy} ${orderBy}`    
-    // return db.query(queryStr, queryValues).then(({rows}) => {
-    //    if(!rows.length){
-    //     return Promise.reject({status: 404, msg: "Request Not Found"})
-    //    } else {
-    //        return rows
-    //    }
-    // })
 }
 
 exports.selectComments = (id) => {
